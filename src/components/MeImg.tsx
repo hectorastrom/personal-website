5073774282bf73e0992c2d782337b4952c989735
Hector Astrom <hector.astrom@gmail.com>
2024-06-10 20:16:22 +0200

    memoized photo, hopefully elim squishing effect on page change

--- src/components/MeImg.tsx
+++ src/components/MeImg.tsx
@@ -1,21 +1,23 @@
-import Link from "next/link";
+import Lin
+k from "next/link";
 import Image from "next/image";
+import { memo } from "react";
 
-export default function MeImg() {
-    return (
-      <>
-        <div className="relative w-24 h-24 sm:w-28 sm:h-28 md:w-56 md:h-56">
-          <Link href="/">
-            <Image
-              src="/images/me.png"
-              alt="Picture of Hector from September 2023"
-              width={500} //arbitrary
-              height={500}
-              className="object-contain w-full h-full"
-              priority={true}
-            />
-          </Link>
-        </div>
-      </>
-    );
+function MeImg() {
+  return (
+    <div className="relative w-24 h-24 sm:w-28 sm:h-28 md:w-56 md:h-56">
+      <Link href="/">
+        <Image
+          src="/images/me.png"
+          alt="Picture of Hector from September 2023"
+          width={500} //arbitrary
+          height={500}
+          className="object-contain w-full h-full"
+          priority={true}
+        />
+      </Link>
+    </div>
+  );
 }
+
+export default memo(MeImg);
