789c4bbd46f18949b5d57a9beb1df7e0b2f33431
Hector Astrom <hector.astrom@gmail.com>
2024-06-13 09:47:33 +0200

    + some mobile landscape adjustments

--- src/components/ResponsiveNav.tsx
+++ src/components/ResponsiveNav.tsx
@@ -4,7 +4,7 @@ import TopNav from "./TopNav";
 export default function ResponsiveNav() {
   return (
     <>
-      <div className="hidden md:block fixed md:relative left-4 md:left-0">
+      <div className="hidden md:block fixed md:relative left-4 md:left-0 ml:-mt-12">
         <SideNav />
       </div>
       <div className="md:hidden w-screen fixed top-0 left-0 z-10">
