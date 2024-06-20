4003eccee04c3d87e0f8e3975c92c7093840796d
Hector Astrom <hector.astrom@gmail.com>
2024-06-20 11:16:00 +0200

    + conditional project SS rendering

--- src/components/HorDivider.tsx
+++ src/components/HorDivider.tsx
@@ -1,7 +1,11 @@
-export default function HorDivider() {
+interface HorDividerProps {
+  className? : string
+}
+
+export default function HorDivider({className} : HorDividerProps) {
     return (
       <>
-        <hr className="my-4 sm:my-2 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-500 to-transparent opacity-50 dark:via-neutral-400" />
+        <hr className={`${className} my-4 sm:my-2 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-500 to-transparent opacity-50 dark:via-neutral-400`} />
       </>
     );
 }
