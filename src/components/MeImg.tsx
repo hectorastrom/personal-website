25d88a21d691d04d8dbbc8c0c0bc33ff343188f4
Hector Astrom <hector.astrom@gmail.com>
2024-08-01 16:43:53 +0200

    little note to self

--- src/components/MeImg.tsx
+++ src/components/MeImg.tsx
@@ -7,7 +7,7 @@ export default function MeImg() {
     <div className="relative w-24 h-24 sm:w-28 sm:h-28 md:w-56 md:h-56">
       <Link href="/">
         <Image
-          src={profileImg}
+          src={profileImg} // static import fixed image jumping/resizing in production version!
           alt="Picture of Hector from September 2023"
           width={406}
           height={461}
