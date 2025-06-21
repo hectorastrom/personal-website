86f160fd08b41173e78176cbc2d75ddfd229e936
hectorastrom <hector.astrom@gmail.com>
2025-06-21 12:14:12 -0700

    - old fonts

--- public/fonts/fonts.tsx
+++ public/fonts/fonts.tsx
@@ -1,9 +1,15 @@
 import { Inter } from "next/font/google";
 import localFont from "next/font/local";
 
-export const bodyFont = Inter({ subsets: ["latin"], weight: ["400", "500", "600", "700", "800"] });
-// export const titleFont = localFont({ src: "Caudex-Bold.ttf", weight: "700"
-// }); 
-// title font is specified in globals.css, so it can be referenced using
-// font-title in tailwind
-export const regards = localFont({ src: "regards.ttf" });
+export const bodyFont = Inter({ 
+  subsets: ["latin"], 
+  weight: ["400", "500", "600", "700", "800"],
+  display: 'swap',
+  preload: true
+});
+
+export const titleFont = localFont({ 
+  src: "gochihand.ttf",
+  display: 'swap',
+  preload: true
+});
